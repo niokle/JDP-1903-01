4eca28a1dd963eae8e8c6d5ff69b7a1f299998c8
niokle <myszapl@gmail.com>
2019-04-15 19:49:26 +0200

    first version of filterFunction

--- src/main/java/com/kodilla/ecommercee/product/service/ProductService.java
+++ src/main/java/com/kodilla/ecommercee/product/service/ProductService.java
@@ -4,9 +4,10 @@ import com.kodilla.ecommercee.product.domain.Product;
 import com.kodilla.ecommercee.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 @Service
 public class ProductService {
@@ -28,5 +29,58 @@ public class ProductService {
 
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
+    }
+
+    public List<Product> findProducts(Long id, String partOfName, String partOfDescription, Double priceMin, Double priceMax) {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .filter(product -> filterFunction(product, id, partOfName, partOfDescription, priceMin, priceMax))
+                .collect(Collectors.toList());
+    }
+
+    private static boolean filterFunction(Product product, Long id, String partOfName, String partOfDescription, Double priceMin, Double priceMax) {
+        boolean idResult;
+        boolean partOfNameResult;
+        boolean partOfDescriptionResult;
+        boolean priceMinResult;
+        boolean priceMaxResult;
+
+        if (id == null) {
+            idResult = true;
+        } else {
+            idResult = product.getId() == id;
+        }
+
+        if (partOfName == null) {
+            partOfNameResult = true;
+        } else {
+            partOfNameResult = product.getName().contains(partOfName);
+        }
+
+        if (partOfDescription == null) {
+            partOfDescriptionResult = true;
+        } else {
+            partOfDescriptionResult = product.getDescription().contains(partOfDescription);
+        }
+
+        if (priceMin == null && priceMax == null) {
+            priceMinResult = true;
+            priceMaxResult = true;
+        } else if (priceMin != null && priceMax != null) {
+            priceMinResult = product.getPrice() >= priceMin;
+            priceMaxResult = product.getPrice() <= priceMax;
+        } else {
+            if (priceMin == null) {
+                priceMinResult = true;
+            } else {
+                priceMinResult = product.getPrice() >= priceMin;
+            }
+            if (priceMax == null) {
+                priceMaxResult = true;
+            } else {
+                priceMaxResult = product.getPrice() >= priceMax;
+            }
+        }
+        return idResult && partOfNameResult && partOfDescriptionResult && priceMinResult && priceMaxResult;
     }
 }
