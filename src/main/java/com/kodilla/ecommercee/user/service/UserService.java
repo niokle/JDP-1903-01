b643dd87dd467dcc4002396bb2b1dca78b05b750
karol-piotrowski <kp0@o2.pl>
2019-04-19 01:08:13 +0200

    UserService added

---
+++ src/main/java/com/kodilla/ecommercee/user/service/UserService.java
@@ -0,0 +1,31 @@
+package com.kodilla.ecommercee.user.service;
+
+import com.kodilla.ecommercee.user.domain.User;
+import com.kodilla.ecommercee.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUser(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
