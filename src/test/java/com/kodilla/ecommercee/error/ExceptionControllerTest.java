e33392a2da486dc7691ea0d24759d4eea3e47e1f
niokle <myszapl@gmail.com>
2019-04-23 21:01:22 +0200

    first commit

---
+++ src/test/java/com/kodilla/ecommercee/error/ExceptionControllerTest.java
@@ -0,0 +1,14 @@
+package com.kodilla.ecommercee.error;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExceptionControllerTest {
+
+    public void test() {
+
+    }
+}
