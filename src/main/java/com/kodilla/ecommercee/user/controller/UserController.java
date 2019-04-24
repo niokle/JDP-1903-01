c6c0b6972df81282116c2b600e2169cbf79042d6
karol-piotrowski <kp0@o2.pl>
2019-04-24 02:47:25 +0200

    UserController and tests added

--- src/main/java/com/kodilla/ecommercee/user/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/user/controller/UserController.java
@@ -1,25 +1,44 @@
 package com.kodilla.ecommercee.user.controller;
 
 
+import com.kodilla.ecommercee.user.domain.User;
 import com.kodilla.ecommercee.user.dto.UserDto;
+import com.kodilla.ecommercee.user.exception.UserNotFoundException;
+import com.kodilla.ecommercee.user.mapper.UserMapper;
+import com.kodilla.ecommercee.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("ecommercee/users")
 public class UserController {
-    @PostMapping
-    public Long createUser(@RequestBody String userName) {
-        UserDto userDto = new UserDto(1L, userName, "1", 12345L);
-        return userDto.getUserId();
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserMapper userMapper;
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    public Long createUser(@RequestBody UserDto userDto) {
+        return userService.saveUser(userMapper.mapUserDtoToUser(userDto)).getUserId();
     }
 
     @PutMapping(path = "/blockUser")
-    public Long blockUser(@RequestParam Long userId) {
-        return userId;
+    public Long blockUser(@RequestParam Long userId) throws UserNotFoundException {
+        User user = userService.getUser(userId).orElseThrow(UserNotFoundException::new);
+        user.setStatus("0");
+        return userService.saveUser(user).getUserId();
     }
 
-    @GetMapping(path = "/generateKey")
-    public Long generateKey(@RequestParam Long userId) {
-        return 12345678L;
+    @PutMapping(path = "/generateKey")
+    public Long generateKey(@RequestParam Long userId) throws UserNotFoundException {
+        User user = userService.getUser(userId).orElseThrow(UserNotFoundException::new);
+        user.setUserKey((long) new Random().nextInt(100000));
+        return userService.saveUser(user).getUserId();
     }
 }
--- src/test/java/com/kodilla/ecommercee/user/controller/UserControllerTests.java
+++ src/test/java/com/kodilla/ecommercee/user/controller/UserControllerTests.java
@@ -1,12 +1,18 @@
 package com.kodilla.ecommercee.user.controller;
 
+import com.kodilla.ecommercee.user.domain.User;
+import com.kodilla.ecommercee.user.dto.UserDto;
+import com.kodilla.ecommercee.user.exception.UserNotFoundException;
+import com.kodilla.ecommercee.user.service.UserService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserControllerTests {
@@ -14,22 +20,71 @@ public class UserControllerTests {
     @Autowired
     private UserController userController;
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void testCreateUser() {
         // given
+        User user1 = new User("User 1", "1", 0L);
+        User user2 = new User("User 2", "1", 0L);
+        UserDto userDto1 = userController.userMapper.mapUserToUserDto(user1);
+        UserDto userDto2 = userController.userMapper.mapUserToUserDto(user2);
+
         // when
-        long result = userController.createUser("James Bond");
+        Long userId1 = userController.createUser(userDto1);
+        Long userId2 = userController.createUser(userDto2);
+
         // then
-        Assert.assertEquals(1, result);
+        Assert.assertEquals(2, userService.getUsers().size());
+
+        //cleanup
+        userService.deleteUser(userId1);
+        userService.deleteUser(userId2);
     }
+
+    @Test
+    public void testBlockUser() {
+        // given
+        User user = new User("User 1", "1", 0L);
+        UserDto userDto = userController.userMapper.mapUserToUserDto(user);
+
+        // when
+        Long userId = userController.createUser(userDto);
+        try {
+            userController.blockUser(userId);
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // then
+        Assert.assertEquals("0", userService.getUser(userId).get().getStatus());
+
+        //cleanup
+        userService.deleteUser(userId);
+    }
+
 
     @Test
     public void testGenerateKey() {
         // given
+        User user = new User("User 1", "1", 0L);
+        UserDto userDto = userController.userMapper.mapUserToUserDto(user);
+
         // when
-        long key = userController.generateKey(23L);
+        Long userId = userController.createUser(userDto);
+        try {
+            userController.generateKey(userId);
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+        long userKey = userService.getUser(userId).get().getUserKey();
+
         // then
-        Assert.assertEquals(12345678L, key);
+        Assert.assertNotEquals(0L, userKey);
+
+        //cleanup
+        userService.deleteUser(userId);
 
     }
 
